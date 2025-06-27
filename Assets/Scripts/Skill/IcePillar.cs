e51d43534088eec7ca9e998db3029d37f8ab2758
Yj621 <wjddbswl26@gmail.com>
2025-06-27 23:36:23 +0900

    Freeze 하는중..^^

--- Assets/Scripts/Skill/IcePillar.cs
+++ Assets/Scripts/Skill/IcePillar.cs
@@ -1,7 +1,25 @@
 using UnityEngine;
 using System.Collections;
-public class IcePillar : AreaDamageSkill
+public class IcePillar : SkillEffect
 {
     protected override string SkillName => "IcePillar";
     protected override Color GizmoColor => Color.cyan;
+
+    protected override IEnumerator SkillRoutine()
+    {
+        var mask = LayerMask.GetMask(TargetLayer);
+        var hits = Physics2D.OverlapCircleAll(transform.position, skillData.radius, mask);
+        foreach (var hit in hits)
+        {
+            var enemy = hit.GetComponent<EnemyMove>();
+            if (enemy != null)
+            {
+                // 적에게 피해를 주고, 일정 시간 동안 얼음 기둥에 묶음
+                enemy.EnemyHurt(skillData.damage);
+                enemy.Freeze(5f); // 1.5초 동안 동결
+            }
+        }
+        yield return new WaitForSeconds(skillData.lifetime);
+        Cleanup();
+    }
 }
